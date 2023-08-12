f7b3b98aa23becbe425db9a45ad4fe6b4b76924b
argenalmaz00 <argenalmaz00@gmail.com>
2023-08-12 16:29:25 +0600

    none

--- app/src/main/java/com/example/myapplication12/ui/MainActivity.kt
+++ app/src/main/java/com/example/myapplication12/ui/MainActivity.kt
@@ -16,7 +16,7 @@ import com.example.myapplication12.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var  binding :ActivityMainBinding
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
